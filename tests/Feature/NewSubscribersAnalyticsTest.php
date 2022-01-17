49ae1c892b0db582b745b224c57f54c88449f93b
Evgeny Epifanov <workeev@gmail.com>
2022-01-17 06:16:59 +0000

    new subscribers analytics tests

--- tests/Feature/NewSubscribersAnalyticsTest.php
+++ tests/Feature/NewSubscribersAnalyticsTest.php
@@ -74,7 +74,6 @@ class NewSubscribersAnalyticsTest extends TestCase
         $labels = [];
 
         for ($i = $daysCount; $i >= 0; $i--) {
-//            array_push($values, $daysCount - $i + 1);
             array_push($labels, $now->subDays($i)->format("d-m-Y"));
         }
 
@@ -213,19 +212,164 @@ class NewSubscribersAnalyticsTest extends TestCase
             ],
         ];
 
-        $now = Carbon::now()->toImmutable();
-        $start = $now->subYear()->toImmutable();
+        $this->assertEquals($values, $response->json());
+    }
 
-        $monthsCount = $start->diffInMonths($now);
+    public function testTableMonthAgo()
+    {
+        $this->seed([
+            AdminSeeder::class,
+            SubscriberByDaySeeder::class,
+        ]);
 
-        $labels = [
-            $start->format("m-Y"),
+        $admin = Admin::where("email", "workeev@gmail.com")->first();
+
+        $response = $this->actingAs($admin)
+            ->withHeaders([
+                "Accept" => "application/json",
+            ])
+            ->get("/api/analytics/newSubscribersTable?key=month");
+
+        $values = [
+            [
+                "count" => 2112,
+                "payload" => "",
+            ],
+            [
+                "count" => 1584,
+                "payload" => "google",
+            ],
+            [
+                "count" => 1056,
+                "payload" => "yandex",
+            ],
+            [
+                "count" => 528,
+                "payload" => "facebook",
+            ],
         ];
 
-        for ($i = 1; $i <= $monthsCount; $i++) {
-            array_push($labels, $start->addMonths($i)->format("m-Y"));
-        }
+        $this->assertEquals($values, $response->json());
+    }
 
-        $this->assertEquals($response->json(), $values);
+    public function testTableWeekAgo()
+    {
+        $this->seed([
+            AdminSeeder::class,
+            SubscriberByDaySeeder::class,
+        ]);
+
+        $admin = Admin::where("email", "workeev@gmail.com")->first();
+
+        $response = $this->actingAs($admin)
+            ->withHeaders([
+                "Accept" => "application/json",
+            ])
+            ->get("/api/analytics/newSubscribersTable?key=week");
+
+        $values = [
+            [
+                "count" => 812,
+                "payload" => "",
+            ],
+            [
+                "count" => 609,
+                "payload" => "google",
+            ],
+            [
+                "count" => 406,
+                "payload" => "yandex",
+            ],
+            [
+                "count" => 203,
+                "payload" => "facebook",
+            ],
+        ];
+
+        $this->assertEquals($values, $response->json());
+    }
+
+    public function testTableCustomByDay()
+    {
+        $this->seed([
+            AdminSeeder::class,
+            SubscriberByDaySeeder::class,
+        ]);
+
+        $admin = Admin::where("email", "workeev@gmail.com")->first();
+
+        $now = Carbon::now()->toImmutable();
+
+        $start = $now->subDays(10)->toImmutable();
+        $end = $now->subDays(5)->toImmutable();
+
+        $response = $this->actingAs($admin)
+            ->withHeaders([
+                "Accept" => "application/json",
+            ])
+            ->get("/api/analytics/newSubscribersTable?key=customPeriod&from={$start->toDateTimeString()}&to={$end->toDateTimeString()}&step=day");
+
+        $values = [
+            [
+                "count" => 588,
+                "payload" => "",
+            ],
+            [
+                "count" => 441,
+                "payload" => "google",
+            ],
+            [
+                "count" => 294,
+                "payload" => "yandex",
+            ],
+            [
+                "count" => 147,
+                "payload" => "facebook",
+            ],
+        ];
+
+        $this->assertEquals($values, $response->json());
+    }
+
+    public function testTableCustomByMonth()
+    {
+        $this->seed([
+            AdminSeeder::class,
+            SubscriberSeeder::class,
+        ]);
+
+        $admin = Admin::where("email", "workeev@gmail.com")->first();
+
+        $now = Carbon::now()->toImmutable();
+
+        $start = $now->subMonths(6)->startOfMonth()->toImmutable();
+        $end = $now->subMonths(1)->endOfMonth()->toImmutable();
+
+        $response = $this->actingAs($admin)
+            ->withHeaders([
+                "Accept" => "application/json",
+            ])
+            ->get("/api/analytics/newSubscribersTable?key=customPeriod&from={$start->toDateTimeString()}&to={$end->toDateTimeString()}&step=month");
+
+        $values = [
+            [
+                "count" => 342,
+                "payload" => "",
+            ],
+            [
+                "count" => 171,
+                "payload" => "google",
+            ],
+            [
+                "count" => 114,
+                "payload" => "yandex",
+            ],
+            [
+                "count" => 57,
+                "payload" => "facebook",
+            ],
+        ];
+
+        $this->assertEquals($values, $response->json());
     }
 }
